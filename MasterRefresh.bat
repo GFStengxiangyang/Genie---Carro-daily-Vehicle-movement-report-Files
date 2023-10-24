d88382975c14a7617b108e72380fcf641b0289e7
GFStengxiangyang <144326956+GFStengxiangyang@users.noreply.github.com>
2023-10-24 21:24:07 +0800

    Update MasterRefresh.bat

--- MasterRefresh.bat
+++ MasterRefresh.bat
@@ -1,7 +1,7 @@
-"C:\ProgramData\miniconda3\python.exe" "C:\Users\Administrator\Documents\GitHub\Genie---Carro-daily-Vehicle-movement-report-Files\Code (Combining 12 vehicle movement report files into 1).py"
+"C:\Users\teng.xiangyang\anaconda3\python.exe" "C:\Users\teng.xiangyang\Documents\GitHub\Genie---Carro-daily-Vehicle-movement-report-Files\Code (Combining 12 vehicle movement report files into 1).py"
 
-"C:\ProgramData\miniconda3\python.exe" "C:\Users\Administrator\Documents\GitHub\Genie---Carro-daily-Vehicle-movement-report-Files\Carro selected field.py"
+"C:\Users\teng.xiangyang\anaconda3\python.exe" "C:\Users\teng.xiangyang\Documents\GitHub\Genie---Carro-daily-Vehicle-movement-report-Files\Carro selected field.py"
 
-"C:\ProgramData\miniconda3\python.exe" "C:\Users\Administrator\Documents\GitHub\Genie---Carro-daily-Vehicle-movement-report-Files\LOOKUP from vehicle to Carro data.py"
+"C:\Users\teng.xiangyang\anaconda3\python.exe" "C:\Users\teng.xiangyang\Documents\GitHub\Genie---Carro-daily-Vehicle-movement-report-Files\LOOKUP from vehicle to Carro data.py"
 
 pause
