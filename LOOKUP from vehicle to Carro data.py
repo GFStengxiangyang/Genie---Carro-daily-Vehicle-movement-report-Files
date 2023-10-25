52edd5406dec0b6c482c316a31ca8f94c752e4e3
GFStengxiangyang <144326956+GFStengxiangyang@users.noreply.github.com>
2023-10-25 09:32:46 +0800

    Update LOOKUP from vehicle to Carro data.py

--- LOOKUP from vehicle to Carro data.py
+++ LOOKUP from vehicle to Carro data.py
@@ -9,23 +9,34 @@ import datetime as dt
 df['disbursement_date'] = pd.to_datetime(df['disbursement_date']).dt.date
 
 
-#vehicle file
+#vehicle file (by vehicle No. look up)
 use_columns=['Vehicle No.','Tx Type.1']
 df1 = pd.read_excel(r"C:/Users/teng.xiangyang/Desktop/Codes and files/Vehicle_movement_report_combined_files.xlsx",usecols = use_columns)
 df1=df1.drop_duplicates(subset=["Vehicle No."], keep="first")
 
+#vehicle file (by chassis no look up)
+use_columns=['Chassis No','Tx Type.1']
+df2 = pd.read_excel(r"C:/Users/teng.xiangyang/Desktop/Codes and files/Vehicle_movement_report_combined_files.xlsx",usecols = use_columns)
+df2=df2.drop_duplicates(subset=["Chassis No"], keep="first")
 
 
-df2 = pd.merge(df, df1, left_on='car_plate', right_on='Vehicle No.',how = "left") 
-df2 = df2.replace(np.nan, 'not in eAuto', regex=True)
+
+
+
+
+
+df3 = pd.merge(df, df1, left_on='car_plate', right_on='Vehicle No.',how = "left") 
+df4 = pd.merge(df3, df2, left_on='chassis_number', right_on='Chassis No',how = "left") 
+
+df4 = df4.replace(np.nan, 'not in eAuto', regex=True)
 import datetime as dt
 today = dt.date.today()
 yesterday = dt.date(today.year,today.month,today.day-1)
-df2 = df2.rename(columns = {'account_status':'account_status' +  format(yesterday), 'Vehicle No.':'car_plate (eAuto)'})
+df4 = df4.rename(columns = {'account_status':'account_status' +  format(yesterday), 'Vehicle No.':'car_plate (eAuto)', 'Chassis No':'Chassis (eAuto)'})
 
 
 output_path = "C:/Users/teng.xiangyang/Desktop/Codes and files/Master_Lookup_Files.xlsx"
 
-df2.to_excel(output_path, index=False)
+df4.to_excel(output_path, index=False)
 
 
